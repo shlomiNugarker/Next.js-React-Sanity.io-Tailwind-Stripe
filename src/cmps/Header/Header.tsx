7c08e470f9666787abf1324484773abb837db193
Shlomi Nugarker <shlomin1231@gmail.com>
2024-03-04 19:47:27 +0200

    add folders

--- src/cmps/Header/Header.tsx
+++ src/cmps/Header/Header.tsx
@@ -1,35 +1,7 @@
 'use client'
 
-import Link from 'next/link'
-
-import Image from 'next/image'
-
 const Header = () => {
-  return (
-    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
-      <div className="flex items-center w-full md:2/3">
-        <Link href="/" className="font-black text-tertiary-dark">
-          Hotelzz
-        </Link>
-        <ul className="flex items-center ml-5">
-          <li className="flex items-center">li</li>
-          <li className="ml-2">li</li>
-        </ul>
-      </div>
-
-      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
-        <li className="hover:-translate-y-2 duration-500 transition-all">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="hover:-translate-y-2 duration-500 transition-all">
-          <Link href="/rooms">Rooms</Link>
-        </li>
-        <li className="hover:-translate-y-2 duration-500 transition-all">
-          <Link href="/">Contact</Link>
-        </li>
-      </ul>
-    </header>
-  )
+  return <header className="">Header</header>
 }
 
 export default Header
