ad7e2904a6d22139506e0bc183a85a65c83aac19
Shlomi Nugarker <shlomin1231@gmail.com>
2024-03-05 20:29:44 +0200

    Header

--- src/cmps/Header/Header.tsx
+++ src/cmps/Header/Header.tsx
@@ -1,7 +1,19 @@
 'use client'
 
 const Header = () => {
-  return <header className="">Header</header>
+  return (
+    <header className="px-5 flex justify-between items-center bg-primary text-white h-14">
+      <div className="flex">
+        <button>search-btn</button>
+        <p>Koshers</p>
+        <p>1-700-12-34-56</p>
+        <span>logo</span>
+        <p>Activity hours</p>
+      </div>
+
+      <div>logo</div>
+    </header>
+  )
 }
 
 export default Header
