4523df26cdfca9f881a1b682c02bca3983e46aa0
Shlomi Nugarker <shlomin1231@gmail.com>
2024-03-06 01:42:09 +0200

    install sanity

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'next-cms',
+
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET as string,
+
+  basePath: '/studio',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
