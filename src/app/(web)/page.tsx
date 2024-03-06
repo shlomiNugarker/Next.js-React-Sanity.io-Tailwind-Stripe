7b9347cfce2f7121e34e0b3747bb5c74bb47f041
Shlomi Nugarker <shlomin1231@gmail.com>
2024-03-07 00:34:19 +0200

    search

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/cmps/HeroSection/HeroSection'
+import PageSearch from '@/cmps/PageSearch/PageSearch'
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <PageSearch />
     </>
   )
 }
