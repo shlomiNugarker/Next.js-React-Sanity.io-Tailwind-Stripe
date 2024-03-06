52432d77f17f23faec151886f2fd3025dc88fed0
Shlomi Nugarker <shlomin1231@gmail.com>
2024-03-07 00:39:19 +0200

    NewsLetter

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,5 +1,6 @@
 import Gallery from '@/cmps/Gallery/Gallery'
 import HeroSection from '@/cmps/HeroSection/HeroSection'
+import NewsLetter from '@/cmps/NewsLetter/NewsLetter'
 import PageSearch from '@/cmps/PageSearch/PageSearch'
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <PageSearch />
       <Gallery />
+      <NewsLetter />
     </>
   )
 }
