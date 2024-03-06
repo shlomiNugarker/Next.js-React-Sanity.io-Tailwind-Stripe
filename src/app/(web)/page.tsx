fe00fb6a9e7d2987438bc5926972c90eaca897f8
Shlomi Nugarker <shlomin1231@gmail.com>
2024-03-07 01:06:40 +0200

    FeaturedRoom, apis, models

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,13 +1,18 @@
+import FeaturedRoom from '@/cmps/FeaturedRoom/FeaturedRoom'
 import Gallery from '@/cmps/Gallery/Gallery'
 import HeroSection from '@/cmps/HeroSection/HeroSection'
 import NewsLetter from '@/cmps/NewsLetter/NewsLetter'
 import PageSearch from '@/cmps/PageSearch/PageSearch'
+import { getFeaturedRoom } from '@/libs/apis'
 
-export default function Home() {
+export default async function Home() {
+  const featuredRoom = await getFeaturedRoom()
+
   return (
     <>
       <HeroSection />
       <PageSearch />
+      <FeaturedRoom featuredRoom={featuredRoom} />
       <Gallery />
       <NewsLetter />
     </>
